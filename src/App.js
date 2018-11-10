00b8172da7711785905ce742889d76a7e81f5cf4
Rashmi Singh <simplyrashmi02@gmail.com>
2018-11-10 19:06:31 +0530

    Committed by Rashmi

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+  import React, { Component } from 'react';
+  import SearchBar from './components/search_bar';
+  import YTSearch from 'youtube-api-search';
+  import VideoList from './components/video_list';
+  import VideoDetail from './components/video_detail';
+  import _ from 'lodash';
+  const API_KEY = 'AIzaSyAgeUXJ6eMXmirNJmF7Hg6rG5ceaBiwO7U';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  class App extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        videos : [],
+        selectedVideo : null
+       };
+      this.videoSearch('React Tutorials');
+    }
+    videoSearch(searchTerm){
+      YTSearch({key:API_KEY ,term:searchTerm}, (data)=>{
+        this.setState({ videos:data, selectedVideo:data[0]});
+      });
+    }
+    render() {
+     const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 300);
+
+      return (
+        <div>
+          <SearchBar onSearchTermChange = {videoSearch} />
+          <VideoDetail video = {this.state.selectedVideo} />
+          <VideoList
+          onVideoSelect = {selectedVideo =>this.setState( {selectedVideo } )}
+          videos = {this.state.videos} />
+        </div>
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
